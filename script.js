7789c7998f5d7979378745b66fd7cbebb5b307dd
Skafeendri <hatim.benta2@gmail.com>
2022-11-07 20:58:09 +0100

    Third question

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-let a=parseInt(prompt("Entrez la valeurde a :",0));
+let a=parseInt(prompt("Entrez la valeur de a :",0));
 let b=parseInt(prompt("Entrez la valeur de b :", 0));
-let c=parseInt(prompt("Entrez la valeur de b :", 0));
+let c=parseInt(prompt("Entrez la valeur de c :", 0));
 function f(){
-    if (a*b*c>0)
-        alert("+");
-        else if (a*b*c<0)
-            alert("-");
-            else
-                alert("No sign");
+    if (a>b & b>c)
+        alert(a+","+b+","+c);
+        else if (a>c & c>b)
+            alert(a+","+c+","+b);
+            else if (b>a & a>c)
+                alert(b+","+a+","+c);
+                else if (b>c &c>a)
+                    alert(b+","+c+","+a);
+                    else if (c>a & a>b)
+                        alert(c+","+a+","+b);
+                            else if (c>a & b>a)
+                                 alert(c+","+b+","+a);
 }
-f();
+f()

23de53e96e957ab15086ef533c093529ecffef13
Skafeendri <hatim.benta2@gmail.com>
2022-11-07 21:04:55 +0100

    Fourth question

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 let a=parseInt(prompt("Entrez la valeur de a :",0));
 let b=parseInt(prompt("Entrez la valeur de b :", 0));
 let c=parseInt(prompt("Entrez la valeur de c :", 0));
-function f(){
-    if (a>b & b>c)
-        alert(a+","+b+","+c);
-        else if (a>c & c>b)
-            alert(a+","+c+","+b);
-            else if (b>a & a>c)
-                alert(b+","+a+","+c);
-                else if (b>c &c>a)
-                    alert(b+","+c+","+a);
-                    else if (c>a & a>b)
-                        alert(c+","+a+","+b);
-                            else if (c>a & b>a)
-                                 alert(c+","+b+","+a);
-}
-f()
+let d=parseInt(prompt("Entrez la valeur de d :", 0));
+let e=parseInt(prompt("Entrez la valeur de e :", 0));
+let m=Math.max(a,b,c,d,e);
+    function f()
+        {
+            alert(m);
+        }
+        f();

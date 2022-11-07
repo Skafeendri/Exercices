ad63ff7dd92a6fcc810da7b9b5aeed0d55c43ddd
Skafeendri <hatim.benta2@gmail.com>
2022-11-07 21:19:01 +0100

    Fifth question

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-let a=parseInt(prompt("Entrez la valeur de a :",0));
-let b=parseInt(prompt("Entrez la valeur de b :", 0));
-let c=parseInt(prompt("Entrez la valeur de c :", 0));
-let d=parseInt(prompt("Entrez la valeur de d :", 0));
-let e=parseInt(prompt("Entrez la valeur de e :", 0));
-let m=Math.max(a,b,c,d,e);
-    function f()
-        {
-            alert(m);
-        }
-        f();
+
+for (let i = 0; i <16; i++)
+{
+    if (i%2==0)
+     alert(i+" est pair")
+     else
+      alert(i+" est impair")
+    
+}

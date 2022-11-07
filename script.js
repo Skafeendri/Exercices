f9e9038bb654a82060dce12c72c9784c1bb7a3ab
Skafeendri <hatim.benta2@gmail.com>
2022-11-07 20:55:56 +0100

    Seecond question

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let a=parseInt(prompt("Entrez la valeurde a :",0));
 let b=parseInt(prompt("Entrez la valeur de b :", 0));
+let c=parseInt(prompt("Entrez la valeur de b :", 0));
 function f(){
-    if (a>b)
-        alert(a);
-        else if (a==b)
-            alert('A egal B');
+    if (a*b*c>0)
+        alert("+");
+        else if (a*b*c<0)
+            alert("-");
             else
-                alert(b);
+                alert("No sign");
 }
 f();

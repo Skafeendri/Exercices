4f135b4d1e48ff0d357c6a080ab8980f13293aa5
Skafeendri <hatim.benta2@gmail.com>
2022-11-07 20:52:28 +0100

    First queestion

---
+++ script.js
@@ -0,0 +1,11 @@
+let a=parseInt(prompt("Entrez la valeurde a :",0));
+let b=parseInt(prompt("Entrez la valeur de b :", 0));
+function f(){
+    if (a>b)
+        alert(a);
+        else if (a==b)
+            alert('A egal B');
+            else
+                alert(b);
+}
+f();
